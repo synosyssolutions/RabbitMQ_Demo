f575def1172a82e9b75c7af9b1a2e109a7bd9c39
synosyssolutions <68543610+synosyssolutions@users.noreply.github.com>
2024-03-11 21:41:11 +0530

    Add files via upload
    C# RabbitMQ demo project

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string ExchangeName = "Demo Exchange";
+string routingKey = "demo-routing-key";
+string queueName = "demo-queue-name";
+
+channel.ExchangeDeclare(ExchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, ExchangeName, routingKey, null);
+
+
+for (int i = 0; i < 60; i++)
+{
+    Console.WriteLine($"sending message #{i}");
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message : #{i}");
+    channel.BasicPublish(ExchangeName,routingKey,null,messageBodyBytes);
+    Thread.Sleep(1000);
+}
+
+channel.Close();
+cnn.Close();
